174c874cc31793caf330d2e23a70a45245302a2e
AryaShah-ComputerEngineer <25ce107@charusat.edu.in>
2025-11-19 23:48:18 +0530

    Add files via upload

---
+++ prac 8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    scanf("%d", &age);
+    if(age >= 18) {
+        printf("Eligible for Savings Account\n");
+    } else {
+        printf("Not Eligible\n");
+    }
+    printf("25ce097\n Manthan");
+    return 0;
+}
