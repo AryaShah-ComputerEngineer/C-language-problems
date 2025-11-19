8c7096994e080f31b823b9e392abb923ef0b17f8
AryaShah-ComputerEngineer <25ce107@charusat.edu.in>
2025-11-19 23:37:49 +0530

    Add files via upload

---
+++ prac 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    double length = 70;
+    double breadth = 90;
+
+    double area = length * breadth;
+    double perimeter = 2 * (length + breadth);
+
+    printf("Area: %.2f\n", area);
+    printf("Perimeter: %.2f\n", perimeter);
+
+printf("25ce097\n Manthan");
+
+    return 0;
+}
