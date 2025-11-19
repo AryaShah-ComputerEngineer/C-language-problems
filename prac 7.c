40ba7f8dff7a6de58fdcae6f4900594dee207390
AryaShah-ComputerEngineer <25ce107@charusat.edu.in>
2025-11-19 23:48:00 +0530

    Add files via upload

---
+++ prac 7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    int ticket_price = 50;
+    scanf("%d", &age);
+    if(age < 12) {
+        printf("Free Entry\n");
+    } else {
+        printf("Ticket Price: %d\n", ticket_price);
+    }
+    printf("25ce097\n Manthan");
+    return 0;
+}
