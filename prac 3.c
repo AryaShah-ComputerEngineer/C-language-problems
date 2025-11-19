b5c01faa4777d7a6a545eef72d4eb4937a8fc121
AryaShah-ComputerEngineer <25ce107@charusat.edu.in>
2025-11-19 23:38:27 +0530

    Add files via upload

---
+++ prac 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float weight, height, bmi;
+
+    scanf("%f", &weight);
+    scanf("%f", &height);
+
+    bmi = weight / (height * height);
+
+    printf("BMI: %.2f\n", bmi);
+
+    printf("25ce097\n Manthan");
+    return 0;
+}
